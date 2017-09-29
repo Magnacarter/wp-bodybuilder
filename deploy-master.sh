2fbae86655af2515a193b306e90f12ea661b363e
Adam.Carter <adamkristopher@gmail.com>
2017-09-29 14:26:50 -0500

    travis test 8

--- deploy-master.sh
+++ deploy-master.sh
@@ -4,18 +4,14 @@ HOST=107.180.50.244
 USER=adamkcarter
 PASS=Wingchun78
 
-sudo apt-get install ncftp
-
-ncftp -u $USER -p $PASS $HOST << EOF
+spawn ssh $USER@$HOST
+expect "password:"
+sleep 1
+send $PASS
 
 cd /public_html/plugin-testing/wp-content/plugins
 
 rm -rf wp-bodybuilder
 
 put -R wp-bodybuilder
-
-# End FTP Connection
-exit
-
-EOF
 
