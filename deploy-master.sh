66a2838489ff56e76a31a8013eaa210c719816c2
Adam.Carter <adamkristopher@gmail.com>
2017-09-29 12:22:54 -0500

    travis test 4

--- deploy-master.sh
+++ deploy-master.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+FTP_USER=adam@adamkristopher.com
+FTP_PASSWORD=Wingchun78
 
+find wp-bodybuilder -type f -exec curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://107.180.50.244/plugin-testing/wp-content/plugins/wp-bodybuilder/{} \;
